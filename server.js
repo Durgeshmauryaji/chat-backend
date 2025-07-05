e3a9f68bddb3fae09f880a85f09b847020179d34
Durgesh Kumar <dkmaurya3241@gmail.com>
2025-07-05 12:15:37 +0530

    🔧 Update CORS origin for Netlify frontend

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"], 
-  }
+  origin: "https://chattingappp.netlify.app", 
+  methods: ["GET", "POST"]
+}
+
 });
 
 io.on("connection", (socket) => {
